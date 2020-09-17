7638a341195b6cc8e66c48ba94c33302c8f71fdf
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-09-17 10:14:37 -0300

    Arreglo filtros

--- bin/filter_dataset.py
+++ bin/filter_dataset.py
@@ -1,39 +1,51 @@
+from tqdm.auto import tqdm
 import fire
+import spacy
 import pandas as pd
-import unidecode
 
 
-def filter_dataset(input_file, output, filters=None):
+def filter_dataset(input_file, output):
     """
     Filtra el dataset de entrada usando las palabras filtro
     """
 
 
-    df = pd.read_excel(input_file, header=0, names=["medio", "fecha", "titulo"])
+    if input_file.endswith("xlsx"):
+        df = pd.read_excel(input_file, header=0, names=["medio", "fecha", "titulo"])
+    else:
+        df = pd.read_csv(input_file)
+
     df["medio"] = df["medio"].str.lower()
     print(f"Tenemos {df.shape[0]} noticias")
     print(df["medio"].value_counts())
 
-    palabras_clave = [
-        "Alberto", "Macri", "Mauricio", "CFK", "Cristina", "Del Caño", "Espert",
-        "Centurion", "Pichetto", "Del Pla", "PJ", "peronismo", "FIT", "Cambiemos",
-        "Juntos por el Cambio", "Lavagna", "Urtubey", "Rosales", "Hotton"
-    ]
+    nlp = spacy.load('es_core_news_sm')
+    palabras_clave = {
+        "alberto", "macri", "mauricio", "cfk", "cristina", "caño", "espert",
+        "centurion", "pichetto", "pla", "pj", "peronismo", "fit", "izquierda", "cambiemos",
+        "cambio", "lavagna", "urtubey", "rosales", "hotton"
+    }
 
     print(f"Usando palabras clave: {' - '.join(palabras_clave)}")
-    important_indices = []
-    current_titles = set()
 
-    for i, row in df.iterrows():
-        try:
-            title = unidecode.unidecode(row["titulo"].lower())
-            for clave in palabras_clave:
-                if clave.lower() in title and title not in current_titles:
-                    current_titles.add(title)
-                    important_indices.append(i)
-        except AttributeError as e:
-            print(f"Posición {i+1}")
-            print(e)
+    current_titles = set()
+    important_indices = []
+
+    for i, row in tqdm(df.iterrows(), total=len(df)):
+        #title = unidecode.unidecode(row["titulo"].lower())
+        title = row["titulo"]
+        if type(title) is not str:
+            continue
+
+        if title in current_titles:
+            continue
+        for tok in nlp(title):
+            if tok.text.lower() in palabras_clave:
+                #print(f"{i} -- {tok.text} ---> {title}")
+                important_indices.append(i)
+                current_titles.add(title)
+                break
+
 
     df_politico = df.loc[important_indices]
     print(f"Noticias recuperadas: {df_politico.shape[0]}")
